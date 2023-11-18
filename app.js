0c7c449d3a5cf02f296c8d36e1799d4a1e1bc969
KarthikRam14 <karthikram1441@gmail.com>
2023-11-18 17:01:10 +0530

    updated

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // Iteration 1: Making the play button in the index.html functional.
 // Description: When the play button is clicked the game.html page should be opened
+
+const play = document.getElementById("play-button")
+play.addEventListener("click", () => {
+    location.href="./game.html"   //vs code can understand without ./ because it is same folder, but git hub understands this command to get into a folder, and if the folder is in different folder as that of the current folder then we use ../
+})
+
+
+
